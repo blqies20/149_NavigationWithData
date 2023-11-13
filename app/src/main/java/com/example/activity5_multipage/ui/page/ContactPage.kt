9643de501d0212697fd792451a1cce410be5777f
ACER <ACER@DESKTOP-QH5VR0V>
2023-11-13 18:21:34 +0700

    add data var dan outlinedtext

--- app/src/main/java/com/example/activity5_multipage/ui/page/ContactPage.kt
+++ app/src/main/java/com/example/activity5_multipage/ui/page/ContactPage.kt
@@ -3,20 +3,53 @@ package com.example.activity5_multipage.ui.page
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.activity5_multipage.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactPage(){
+
+    var namaTxt by remember{ mutableStateOf("") }
+    var alamatTxt by remember{ mutableStateOf("") }
+    var tlpTxt by remember{ mutableStateOf("") }
+
+    var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,tlpTxt)
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxWidth()
     ) {
         OutlinedTextField(
-            value ="" ,
-            onValueChange = "",)
+            value = namaTxt,
+            onValueChange = {namaTxt = it},
+            label = { Text(text = stringResource(id = R.string.nama)
+            )}
+        )
+
+        OutlinedTextField(
+            value = alamatTxt,
+            onValueChange = {alamatTxt = it},
+            label = { Text(text = stringResource(id = R.string.alamat)
+            )}
+        )
+
+        OutlinedTextField(
+            value = tlpTxt,
+            onValueChange = {tlpTxt = it},
+            label = { Text(text = stringResource(id = R.string.tlp)
+            )}
+        )
     }
 }
